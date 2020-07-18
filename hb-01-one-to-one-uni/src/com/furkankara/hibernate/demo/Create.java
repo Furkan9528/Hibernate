837d702f99823b51e954cad313d97580134ee7b0
Furkan KARA - Junior Java Developer <58552366+Furkan9528@users.noreply.github.com>
2020-07-18 10:11:32 +0200

    One to One - CRUD
    One to One - CRUD

---
+++ hb-01-one-to-one-uni/src/com/furkankara/hibernate/demo/Create.java
@@ -0,0 +1,57 @@
+package com.furkankara.hibernate.demo;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.furkankara.hibernate.demo.entity.Instructor;
+import com.furkankara.hibernate.demo.entity.InstructorDetail;
+
+
+public class Create {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+	try {			
+		
+			// create the object
+			Instructor tempInstructor= new Instructor("Furkan","KARA", "furkan@gmail.com");
+			
+			InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.furkan.com","Luv 2 code!!"); 
+			
+			//associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+		
+			// start a transaction
+			session.beginTransaction();
+			
+			//save the instructor
+			//Note: this will ALSO save the details object
+			//beacuse of CascadeType.ALL
+			//
+			System.out.println("Saving instructor: "+tempInstructor);
+			session.save(tempInstructor);
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+	}
+		finally {
+			factory.close();
+		}
+	}
+
+	
+}
