324786194e4db4aab32ae3a5539c82355eb40409
Furkan KARA - Junior Java Developer <58552366+Furkan9528@users.noreply.github.com>
2020-12-13 17:52:42 +0100

    Update InstructorDetail.java

--- hb-05-many-to-many/src/com/furkankara/hibernate/demo/entity/InstructorDetail.java
+++ hb-05-many-to-many/src/com/furkankara/hibernate/demo/entity/InstructorDetail.java
@@ -30,7 +30,7 @@ public class InstructorDetail {
 	@Column(name="id")
 	private int id;
 	
-	@Column(name="youtube_channel")
+	@Column(name="youtube_channel") 
 	private String youtubeChannel;
 	
 	@Column(name="hobby")
